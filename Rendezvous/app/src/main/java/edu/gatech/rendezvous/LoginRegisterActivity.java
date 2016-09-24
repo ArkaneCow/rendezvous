81261ec8cb2e535ad60f07e79bff73abf2dc96f2
ZSchlesinger <zach.schlesinger@gmail.com>
2016-09-24 01:08:33 -0400

    Renamed login activity to LoginRegisterActivity.java and activity_login_register.xml

--- Rendezvous/app/src/main/java/edu/gatech/rendezvous/LoginRegisterActivity.java
+++ Rendezvous/app/src/main/java/edu/gatech/rendezvous/LoginRegisterActivity.java
@@ -3,11 +3,11 @@ package edu.gatech.rendezvous;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginRegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_login_register);
     }
 }
