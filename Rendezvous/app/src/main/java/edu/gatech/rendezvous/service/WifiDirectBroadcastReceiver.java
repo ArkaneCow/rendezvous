a2619563a076b77a0fa909cef44b940e3f2e91f2
Jason Wu <jasonwupilly1997@gmail.com>
2016-09-24 02:57:28 -0400

    work on wifidirectbroadcastreceiver class

--- Rendezvous/app/src/main/java/edu/gatech/rendezvous/service/WifiDirectBroadcastReceiver.java
+++ Rendezvous/app/src/main/java/edu/gatech/rendezvous/service/WifiDirectBroadcastReceiver.java
@@ -3,11 +3,22 @@ package edu.gatech.rendezvous.service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pManager;
+
+import java.util.List;
 
 /**
  * Created by jwpilly on 9/24/16.
  */
 public class WifiDirectBroadcastReceiver extends BroadcastReceiver {
+
+    private WifiDirectBroadcastReceiver ourInstance = null;
+
+    private WifiP2pManager wifiManager;
+    private WifiP2pManager.Channel wifiChannel;
+
+    private List<String> idList;
+
     @Override
     public void onReceive(Context context, Intent intent) {
 
