e091838c925c85a16c2a428d28882c8102998ddb
Jason Wu <jasonwupilly1997@gmail.com>
2016-09-24 05:02:47 -0400

    finish wifidirectservice class

--- Rendezvous/app/src/main/java/edu/gatech/rendezvous/service/WifiDirectService.java
+++ Rendezvous/app/src/main/java/edu/gatech/rendezvous/service/WifiDirectService.java
@@ -1,27 +1,66 @@
 package edu.gatech.rendezvous.service;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.IntentFilter;
 import android.net.wifi.p2p.WifiP2pManager;
+import android.os.Handler;
 
 /**
  * Created by jwpilly on 9/24/16.
  */
 public class WifiDirectService {
+
+    public static final int UPDATE_PERIOD = 10000;
+
     private static WifiDirectService ourInstance = null;
     private static Context wifiContext;
     private WifiP2pManager wifiManager;
     private WifiP2pManager.Channel wifiChannel;
     private WifiDirectBroadcastReceiver wifiDirectBroadcastReceiver;
+    private IntentFilter intentFilter;
+    private BroadcastReceiver receiver = null;
+
+    private Handler updateHandler = new Handler();
+
+    private Runnable discoverRunnable = new Runnable() {
+        @Override
+        public void run() {
+            wifiDiscover();
+            updateHandler.postDelayed(this, UPDATE_PERIOD);
+        }
+    };
 
     private void initializeWifiDirect() {
-
+        intentFilter = new IntentFilter();
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
         wifiManager = (WifiP2pManager) wifiContext.getSystemService(Context.WIFI_P2P_SERVICE);
         wifiChannel = wifiManager.initialize(wifiContext, wifiContext.getMainLooper(), null);
+        receiver = new WifiDirectBroadcastReceiver(wifiManager, wifiChannel);
+        updateHandler.post(discoverRunnable);
+    }
+
+    private void wifiDiscover() {
+        wifiManager.discoverPeers(wifiChannel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+
+            }
+
+            @Override
+            public void onFailure(int reasonCode) {
+
+            }
+        });
     }
 
     private WifiDirectService(Context context) {
         wifiContext = context;
-        // wifiDirectBroadcastReceiver = new WifiDirectBroadcastReceiver();
+        initializeWifiDirect();
+        wifiDirectBroadcastReceiver = new WifiDirectBroadcastReceiver(wifiManager, wifiChannel);
     }
 
     public static WifiDirectService getInstance(Context context) {
