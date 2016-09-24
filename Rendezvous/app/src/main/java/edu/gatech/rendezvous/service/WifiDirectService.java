c1e0357b9d1852f74c0eda8966231f696fb6c8ad
Jason Wu <jasonwupilly1997@gmail.com>
2016-09-24 04:44:12 -0400

    add transparent versions of drawables

--- Rendezvous/app/src/main/java/edu/gatech/rendezvous/service/WifiDirectService.java
+++ Rendezvous/app/src/main/java/edu/gatech/rendezvous/service/WifiDirectService.java
@@ -1,6 +1,7 @@
 package edu.gatech.rendezvous.service;
 
 import android.content.Context;
+import android.net.wifi.p2p.WifiP2pManager;
 
 /**
  * Created by jwpilly on 9/24/16.
@@ -8,10 +9,19 @@ import android.content.Context;
 public class WifiDirectService {
     private static WifiDirectService ourInstance = null;
     private static Context wifiContext;
+    private WifiP2pManager wifiManager;
+    private WifiP2pManager.Channel wifiChannel;
     private WifiDirectBroadcastReceiver wifiDirectBroadcastReceiver;
+
+    private void initializeWifiDirect() {
+
+        wifiManager = (WifiP2pManager) wifiContext.getSystemService(Context.WIFI_P2P_SERVICE);
+        wifiChannel = wifiManager.initialize(wifiContext, wifiContext.getMainLooper(), null);
+    }
 
     private WifiDirectService(Context context) {
         wifiContext = context;
+        // wifiDirectBroadcastReceiver = new WifiDirectBroadcastReceiver();
     }
 
     public static WifiDirectService getInstance(Context context) {
