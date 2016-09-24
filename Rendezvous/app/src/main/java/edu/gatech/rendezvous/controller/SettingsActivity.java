e814eb7f1d150a23c0ac60c2110fd1b8fc269f9f
ZSchlesinger <zach.schlesinger@gmail.com>
2016-09-24 05:00:50 -0400

    Fixed nav bar icons & settings button in nav bar now opens settings

--- Rendezvous/app/src/main/java/edu/gatech/rendezvous/controller/SettingsActivity.java
+++ Rendezvous/app/src/main/java/edu/gatech/rendezvous/controller/SettingsActivity.java
@@ -9,6 +9,6 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_set_reminder);
+        setContentView(R.layout.activity_settings);
     }
 }
