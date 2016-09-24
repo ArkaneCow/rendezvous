7ac884077c4a86e94f4dc9c1b33323617b692a14
Jason Wu <jasonwupilly1997@gmail.com>
2016-09-24 02:29:19 -0400

    updated androidmanifest with needed permissions

--- Rendezvous/app/src/main/java/edu/gatech/rendezvous/service/WifiDirectBroadcastReceiver.java
+++ Rendezvous/app/src/main/java/edu/gatech/rendezvous/service/WifiDirectBroadcastReceiver.java
@@ -1,7 +1,15 @@
 package edu.gatech.rendezvous.service;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
 /**
  * Created by jwpilly on 9/24/16.
  */
-public class WifiDirectBroadcastReceiver {
+public class WifiDirectBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
 }
