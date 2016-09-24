acbb24e0f0438b971f0a41ccf4668bea2892071d
ZSchlesinger <zach.schlesinger@gmail.com>
2016-09-24 04:22:10 -0400

    Customized nav bar with text and icons

--- Rendezvous/app/src/main/java/edu/gatech/rendezvous/controller/MainMenuActivity.java
+++ Rendezvous/app/src/main/java/edu/gatech/rendezvous/controller/MainMenuActivity.java
@@ -130,17 +130,15 @@ public class MainMenuActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_plan) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_add) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_friends) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_rendez) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_settings) {
 
         }
 
