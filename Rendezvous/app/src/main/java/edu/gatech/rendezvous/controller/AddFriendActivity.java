e4db677f80218eb1e28b5d0895a7967118c56fd0
Jason Wu <jasonwupilly1997@gmail.com>
2016-09-24 07:21:53 -0400

    made text in listview visible and work on addfriendactivity

--- Rendezvous/app/src/main/java/edu/gatech/rendezvous/controller/AddFriendActivity.java
+++ Rendezvous/app/src/main/java/edu/gatech/rendezvous/controller/AddFriendActivity.java
@@ -1,36 +1,68 @@
 package edu.gatech.rendezvous.controller;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import edu.gatech.rendezvous.R;
 import edu.gatech.rendezvous.service.WifiDirectService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AddFriendActivity extends AppCompatActivity {
 
     private WifiDirectService wifiDirectService;
+    private ListView friendListView;
+    private FriendListAdapter friendListAdapter;
+    private List<String> friendsList = new ArrayList<>();
+
+    private Handler friendUpdater = new Handler();
+    private Runnable friendUpdateRunnable = new Runnable() {
+        @Override
+        public void run() {
+            List<String> newFriends = refreshList();
+            if (newFriends != null) {
+                friendsList = newFriends;
+            }
+            friendListAdapter.updateFriends(friendsList);
+            friendListAdapter.notifyDataSetChanged();
+            friendUpdater.postDelayed(this, WifiDirectService.UPDATE_PERIOD);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_friend);
         wifiDirectService = WifiDirectService.getInstance(getApplicationContext());
+        friendListView = (ListView) findViewById(R.id.friendListView);
+        friendListAdapter = new FriendListAdapter(friendsList, getApplicationContext());
+        friendListView.setAdapter(friendListAdapter);
+        friendUpdater.post(friendUpdateRunnable);
+    }
 
+    private List<String> refreshList() {
+        List<String> idList = wifiDirectService.getWifiDirectBroadcastReceiver().getNameList();
+        if (idList == null) {
+            return null;
+        }
+        return idList;
+    }
+
+    private void updateFriends() {
+        List<String> newFriends = refreshList();
+        if (newFriends != null) {
+            friendsList = newFriends;
+        }
+        friendListAdapter.updateFriends(friendsList);
+        friendListAdapter.notifyDataSetChanged();
     }
 
     public void refreshFriendsNearby(View v) {
-        String s = "";
-        List<String> idList = wifiDirectService.getWifiDirectBroadcastReceiver().getNameList();
-        if (idList == null) {
-            Log.v("refreshFriendsNearby", "null");
-            return;
-        }
-        for (String i : wifiDirectService.getWifiDirectBroadcastReceiver().getNameList()) {
-            s += i + ",";
-        }
-        Log.v("refreshFriendsNearby", s);
+        updateFriends();
     }
 }
--- Rendezvous/app/src/main/java/edu/gatech/rendezvous/controller/FriendListAdapter.java
+++ Rendezvous/app/src/main/java/edu/gatech/rendezvous/controller/FriendListAdapter.java
@@ -1,7 +1,79 @@
 package edu.gatech.rendezvous.controller;
 
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+import edu.gatech.rendezvous.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by jwpilly on 9/24/16.
  */
-public class FriendListAdapter {
+public class FriendListAdapter extends BaseAdapter implements ListAdapter {
+
+    private static LayoutInflater inflater = null;
+    private List<String> friends = new ArrayList<>();
+    private Context context;
+
+    public FriendListAdapter(List<String> friends, Context context) {
+        this.friends = friends;
+        this.context = context;
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    public void updateFriends(List<String> friends) {
+        this.friends.clear();
+        for (String friend : friends) {
+            this.friends.add(friend);
+        }
+        String s = "";
+        for (String f : this.friends) {
+            s += f;
+        }
+        Log.v("updateFriends", s);
+    }
+
+    @Override
+    public int getCount() {
+        return friends.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return friends.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        View newView;
+        if (view == null) {
+            newView = inflater.inflate(R.layout.friend_list_item, null);
+        } else {
+            newView = view;
+        }
+        final TextView listItemText = (TextView) newView.findViewById(R.id.friendLabel);
+        final String friendName = friends.get(i);
+        listItemText.setText(friendName);
+        final Button actionButton = (Button) newView.findViewById(R.id.friendButton);
+        actionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.v("FriendListAdapter", "add " + friendName);
+            }
+        });
+        return newView;
+    }
 }
