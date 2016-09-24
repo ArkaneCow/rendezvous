120e420531275da080edaaf105a9454f6871d832
Jason Wu <jasonwupilly1997@gmail.com>
2016-09-24 00:44:16 -0400

    create android studio project

---
+++ Rendezvous/app/src/main/java/edu/gatech/rendezvous/SetReminderActivity.java
@@ -0,0 +1,13 @@
+package edu.gatech.rendezvous;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SetReminderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_reminder);
+    }
+}
