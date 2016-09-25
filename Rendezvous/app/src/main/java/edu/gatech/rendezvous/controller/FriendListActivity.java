d9ac415530ba4f60d3b94a4ee4b4c56bfec03ce5
Jason Wu <jasonwupilly1997@gmail.com>
2016-09-25 02:45:48 -0400

    work on addfriendactivity listview

--- Rendezvous/app/src/main/java/edu/gatech/rendezvous/controller/FriendListActivity.java
+++ Rendezvous/app/src/main/java/edu/gatech/rendezvous/controller/FriendListActivity.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import android.widget.ListView;
 import edu.gatech.rendezvous.R;
 import edu.gatech.rendezvous.network.ApiCall;
 import edu.gatech.rendezvous.network.ApiCallback;
@@ -18,27 +20,39 @@ import edu.gatech.rendezvous.service.SessionState;
 
 public class FriendListActivity extends AppCompatActivity {
 
+    private ListView friendsList;
+    private UserListAdapter userListAdapter;
+    private List<String> fList = new ArrayList<>();
+
+    private RendezvousCommandFactory rcf;
+    private RendezvousInvoker rci;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends_list);
         getSupportActionBar().hide();
-    }
-
-    private RendezvousCommandFactory rcf;
-    private RendezvousInvoker rci;
-
-    ApiCall rapiCall = new ApiCall(rcf.getFriendListCommand(SessionState.getInstance().getSessionUserName()), new ApiCallback<RendezvousUserListReceiver>() {
-        @Override
-        public void onReceive(RendezvousUserListReceiver receiver) {
-            if (receiver.getEntity().size() != 0) {
-                ArrayList<String> arrList = (ArrayList<String>) receiver.getEntity();
-                ArrayAdapter<String> adapter;
+        rcf = RendezvousCommandFactory.getInstance();
+        rci = RendezvousInvoker.getInstance();
+        friendsList = (ListView) findViewById(R.id.friendListView);
+        userListAdapter = new UserListAdapter(fList, getApplicationContext());
+        friendsList.setAdapter(userListAdapter);
+        ApiCall rapiCall = new ApiCall(rcf.getFriendListCommand(SessionState.getInstance().getSessionUserName()), new ApiCallback<RendezvousUserListReceiver>() {
+            @Override
+            public void onReceive(RendezvousUserListReceiver receiver) {
+                if (receiver.getEntity().size() != 0) {
+                    final List<String> result = receiver.getEntity();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            userListAdapter.updateFriends(result);
+                            userListAdapter.notifyDataSetChanged();
+                        }
+                    });
+                }
             }
-        }
-    });
-
-
-
+        });
+        rci.executeCall(rapiCall);
+    }
 
 }
---
+++ Rendezvous/app/src/main/java/edu/gatech/rendezvous/controller/UserListAdapter.java
@@ -0,0 +1,67 @@
+package edu.gatech.rendezvous.controller;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+import edu.gatech.rendezvous.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by jwpilly on 9/24/16.
+ */
+public class UserListAdapter extends BaseAdapter implements ListAdapter {
+
+    private static LayoutInflater inflater = null;
+    private List<String> users = new ArrayList<>();
+    private Context context;
+
+    public UserListAdapter(List<String> friends, Context context) {
+        this.users = friends;
+        this.context = context;
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    public void updateFriends(List<String> users) {
+        this.users.clear();
+        for (String user : users) {
+            this.users.add(user);
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return users.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return users.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        View newView;
+        if (view == null) {
+            newView = inflater.inflate(R.layout.friend_list_item, null);
+        } else {
+            newView = view;
+        }
+        final TextView listItemText = (TextView) newView.findViewById(R.id.usernameLabel);
+        final String userName = users.get(i);
+        listItemText.setText(userName);
+        return newView;
+    }
+}
